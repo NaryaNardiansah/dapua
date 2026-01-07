abf6763779fc5c2c9f7b0edd6fffbf2800a6323f
NaryaNardiansah <sitimemey58@gmail.com>
2026-01-08 03:48:16 +0700

    Fix: Explicitly register core providers and clean up vendor handling

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    Illuminate\View\ViewServiceProvider::class, // Tambahkan ini secara paksa
+    Illuminate\Events\EventServiceProvider::class, // Tambahkan ini juga
     App\Providers\AppServiceProvider::class,
     App\Providers\SettingsServiceProvider::class,
 ];
